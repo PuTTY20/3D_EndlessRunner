a4c90871844da6c63f5376b6b63ad4de67bd803c
tube0330 <zien0516@naver.com>
2024-09-23 16:39:49 +0900

    Remy Movement

--- Assets/02.Scripts/RemyCtrl.cs
+++ Assets/02.Scripts/RemyCtrl.cs
@@ -5,15 +5,61 @@ using UnityEngine;
 public class RemyCtrl : MonoBehaviour
 {
     Transform tr;
+    Rigidbody rb;
+    Animator ani;
+
+    float moveValue = 0.8f; // 좌우로 한 번에 이동하는 거리
+
+    float moveY => Input.GetAxisRaw("Vertical");
+    float jumpForce = 5.0f;   // 점프 시 힘
+    [SerializeField] bool isGround = true;   // 캐릭터가 바닥에 있는지 여부
+    [SerializeField] bool isSlide = false;   // 슬라이딩 여부
 
     void Start()
     {
-        
         tr = transform;
+        rb = GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // 좌우 입력
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            tr.position += new Vector3(-moveValue, 0, 0);
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            tr.position += new Vector3(moveValue, 0, 0);
+
+        // 상하 입력
+        if (moveY > 0 && isGround) StartCoroutine(Jump());
+        if (moveY < 0 && !isSlide) StartCoroutine(Slide());
+
+    }
+
+    IEnumerator Jump()
+    {
+        ani.SetTrigger("Jump");
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGround = false;
+
+        yield return new WaitForSeconds(0.8f);
+
+        isGround = true;
+    }
+
+    IEnumerator Slide()
+    {
+        ani.SetTrigger("Slide");
+        isSlide = true;
+
+        yield return new WaitForSeconds(1f);
         
+        isSlide = false;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("GROUND"))
+            isGround = true;
     }
 }
