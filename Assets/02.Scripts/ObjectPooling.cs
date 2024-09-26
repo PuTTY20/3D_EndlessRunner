46df1d08c45527a91d8b28694ae24f6cc6fb7667
tube0330 <zien0516@naver.com>
2024-09-26 14:56:17 +0900

    PlatformList들을 담을 List 생성

--- Assets/02.Scripts/ObjectPooling.cs
+++ Assets/02.Scripts/ObjectPooling.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 public class ObjectPooling : MonoBehaviour
 {
     public static ObjectPooling poolingManager;
-    
-    public List<GameObject> platformList = new List<GameObject>();
-    public int poolSize = 30;
-    public GameObject[] platforms; // 랜덤으로 선택할 플랫폼 배열
-    
+
+    public List<GameObject> defaultPlatformList = new List<GameObject>();
+    public List<GameObject> bridgePlatformList = new List<GameObject>();
+    public List<GameObject> woodPlatformList = new List<GameObject>();
+    public List<GameObject> gatePlatformList = new List<GameObject>();
+    public List<GameObject> flagShortPlatformList = new List<GameObject>();
+    public List<GameObject> flagLongPlatformList = new List<GameObject>();
+    public List<GameObject> doorPlatformList = new List<GameObject>();
+    public List<GameObject> onePlatformList = new List<GameObject>();
+    public List<GameObject> twoToOnePlatformList = new List<GameObject>();
+
+    public int poolSize = 60;
+    public int platformSize = 9;
+
+    // List of Lists of GameObjects to hold multiple platform lists
+    public List<List<GameObject>> PlatformList = new List<List<GameObject>>();
+
     [Header("Platform")]
     public GameObject _default;
     public GameObject bridge;
     public GameObject wood;
     public GameObject gate;
-    
+
     [Header("Obstacle")]
     public GameObject flagShort;
     public GameObject flagLong;
     public GameObject door;
-    
+
     [Header("Condition Platform")]
     public GameObject one;
     public GameObject twoToOne;
@@ -34,55 +46,71 @@ public class ObjectPooling : MonoBehaviour
         else if (poolingManager != this)
             Destroy(gameObject);
 
-        StartCoroutine(CreatePlatformPool());
+        StartCoroutine(CreateDefaultPool());
+        StartCoroutine(CreatebridgePool());
     }
 
-    IEnumerator CreatePlatformPool()
+    IEnumerator CreateDefaultPool()
     {
-        GameObject platformGroup = new GameObject("PlatformGroup");
+        GameObject defaultGroup = new GameObject("DefaultGroup");
 
         for (int i = 0; i < poolSize; i++)
         {
-            // 랜덤 플랫폼 선택
-            GameObject randomPlatform = platforms[Random.Range(0, platforms.Length)];
-            var platform = Instantiate(randomPlatform, platformGroup.transform);
-            platform.SetActive(false);
-            platformList.Add(platform);
+            var defaultPlatform = Instantiate(_default, defaultGroup.transform);
+            defaultPlatform.SetActive(false);
+            defaultPlatformList.Add(defaultPlatform);
         }
 
         yield return new WaitForSeconds(0.1f);
     }
 
-    IEnumerator Start()
+    IEnumerator CreatebridgePool()
     {
-        // 모든 플랫폼 활성화
-        for (int i = 0; i < platformList.Count; i++)
+        GameObject bridgeGroup = new GameObject("BridgeGroup");
+
+        for (int i = 0; i < poolSize; i++)
         {
-            if (!platformList[i].activeInHierarchy)
+            var bridgePlatform = Instantiate(bridge, bridgeGroup.transform);
+            bridgePlatform.SetActive(false);
+            bridgePlatformList.Add(bridgePlatform);
+        }
+
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    void Start()
+    {
+        PlatformList.Add(defaultPlatformList);
+        PlatformList.Add(bridgePlatformList);
+
+        StartCoroutine(GetColSize(PlatformList[0]));
+    }
+
+    IEnumerator GetColSize(List<GameObject> selectedPlatform)
+    {
+        for (int i = 0; i < defaultPlatformList.Count; i++)
+        {
+            if (!selectedPlatform[i].activeInHierarchy)
             {
-                platformList[i].SetActive(true);
-                
-                // 플랫폼의 Collider 크기 가져오기
-                Collider platformCollider = platformList[i].GetComponent<Collider>();
-                if (platformCollider != null)
+                selectedPlatform[i].SetActive(true);
+
+                Collider platformCol = selectedPlatform[i].GetComponent<Collider>();
+                if (platformCol != null)
                     // 플랫폼의 크기만큼 위치 조정
-                    lastPlatformPosition += new Vector3(0, 0, platformCollider.bounds.size.z);
-                else
-                    // Collider가 없는 경우 기본 값으로 이동 (예: z축으로 5)
-                    lastPlatformPosition += new Vector3(0, 0, 5.0f);
-                
+                    lastPlatformPosition += new Vector3(0, 0, platformCol.bounds.size.z);
+
                 // 플랫폼 위치 조정
-                platformList[i].transform.position = lastPlatformPosition;
+                selectedPlatform[i].transform.position = lastPlatformPosition;
 
                 // 비활성화 코루틴 호출
-                StartCoroutine(DeactivatePlatform(platformList[i]));
+                StartCoroutine(PlatformOff(selectedPlatform[i]));
             }
 
             yield return new WaitForSeconds(0.1f);
         }
     }
 
-    private IEnumerator DeactivatePlatform(GameObject platform)
+    private IEnumerator PlatformOff(GameObject platform)
     {
         yield return new WaitForSeconds(2f); // 2초 대기
         platform.SetActive(false); // 플랫폼 비활성화
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# 3D_EndlessRunner
+# 240924
+1. Remy Movement 스크립트 작성
+2. Animation 적용
+3. Platform ObjectPooling
+
+# 240926
+1. ObjectPooling된 PlatformList들을 담을 List를 만들어 List에 추가
+
+나중에 할거
+플랫폼들 다 오브젝트 풀링하기
+플랫폼 담긴 이중리스트 Start가 아니라 다른곳에 옮기기 여러번 해야되니까
