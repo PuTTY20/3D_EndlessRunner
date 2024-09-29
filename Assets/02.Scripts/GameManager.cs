a2974adda41e510523ecd32c4d6888544fbc7d11
tube0330 <zien0516@naver.com>
2024-09-29 13:05:47 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> PlatformList = new List<GameObject>();
     Transform tr;
 
     void Start()
     {
         tr = transform;
+        StartCoroutine(ActivatePlatforms());
     }
 
-    
+    IEnumerator ActivatePlatforms()
+    {
+        while (true)
+        {
+            #region ObjectPooling을 사용하지 않을 때
+            // //랜덤하게 하나의 플랫폼을 선택하여 활성화
+            // int randomIndex = Random.Range(0, PlatformList.Count);
+            // GameObject selectedPlatform = Instantiate(PlatformList[randomIndex]);
+            // // 위치 설정
+            // selectedPlatform.transform.position = new Vector3(0, 0, 76f);
+            // selectedPlatform.SetActive(true);
+            #endregion
+
+            // ObjectPooling에서 랜덤하게 하나의 플랫폼을 가져옴
+            GameObject selectedPlatform = ObjectPooling.poolingManager.GetPlatform();
+            selectedPlatform.SetActive(true);
+
+            if (selectedPlatform != null)
+            {
+                // 위치 설정
+                selectedPlatform.transform.position = new Vector3(0, 0, 76f);
+
+                if (selectedPlatform.transform.position == Vector3.zero)
+                    ObjectPooling.poolingManager.RetunPlatformPool(selectedPlatform);
+            }
+
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
 }
--- Assets/02.Scripts/ObjectPooling.cs
+++ Assets/02.Scripts/ObjectPooling.cs
@@ -6,38 +6,13 @@ public class ObjectPooling : MonoBehaviour
 {
     public static ObjectPooling poolingManager;
 
-    public List<GameObject> defaultPlatformList = new List<GameObject>();
-    public List<GameObject> bridgePlatformList = new List<GameObject>();
-    public List<GameObject> woodPlatformList = new List<GameObject>();
-    public List<GameObject> gatePlatformList = new List<GameObject>();
-    public List<GameObject> flagShortPlatformList = new List<GameObject>();
-    public List<GameObject> flagLongPlatformList = new List<GameObject>();
-    public List<GameObject> doorPlatformList = new List<GameObject>();
-    public List<GameObject> onePlatformList = new List<GameObject>();
-    public List<GameObject> twoToOnePlatformList = new List<GameObject>();
-
-    public int poolSize = 60;
-    public int platformSize = 9;
-
-    // List of Lists of GameObjects to hold multiple platform lists
-    public List<List<GameObject>> PlatformList = new List<List<GameObject>>();
-
-    [Header("Platform")]
+    public List<GameObject> PlatformList = new List<GameObject>();
     public GameObject _default;
     public GameObject bridge;
-    public GameObject wood;
-    public GameObject gate;
+    public GameObject oneLeft;
+    public GameObject oneRight;
 
-    [Header("Obstacle")]
-    public GameObject flagShort;
-    public GameObject flagLong;
-    public GameObject door;
-
-    [Header("Condition Platform")]
-    public GameObject one;
-    public GameObject twoToOne;
-
-    private Vector3 lastPlatformPosition = Vector3.zero; // 마지막 플랫폼 위치
+    public int poolSize = 3;
 
     void Awake()
     {
@@ -48,6 +23,8 @@ public class ObjectPooling : MonoBehaviour
 
         StartCoroutine(CreateDefaultPool());
         StartCoroutine(CreatebridgePool());
+        StartCoroutine(CreateOneLeftPool());
+        StartCoroutine(CreateOneRightPool());
     }
 
     IEnumerator CreateDefaultPool()
@@ -58,7 +35,7 @@ public class ObjectPooling : MonoBehaviour
         {
             var defaultPlatform = Instantiate(_default, defaultGroup.transform);
             defaultPlatform.SetActive(false);
-            defaultPlatformList.Add(defaultPlatform);
+            PlatformList.Add(defaultPlatform);
         }
 
         yield return new WaitForSeconds(0.1f);
@@ -72,49 +49,54 @@ public class ObjectPooling : MonoBehaviour
         {
             var bridgePlatform = Instantiate(bridge, bridgeGroup.transform);
             bridgePlatform.SetActive(false);
-            bridgePlatformList.Add(bridgePlatform);
+            PlatformList.Add(bridgePlatform);
         }
 
         yield return new WaitForSeconds(0.1f);
     }
 
-    void Start()
+    IEnumerator CreateOneLeftPool()
     {
-        PlatformList.Add(defaultPlatformList);
-        PlatformList.Add(bridgePlatformList);
+        GameObject oneLeftGroup = new GameObject("OneLeftGroup");
 
-        StartCoroutine(GetColSize(PlatformList[0]));
-    }
-
-    IEnumerator GetColSize(List<GameObject> selectedPlatform)
-    {
-        for (int i = 0; i < selectedPlatform.Count; i++)
+        for (int i = 0; i < poolSize; i++)
         {
-            if (!selectedPlatform[i].activeInHierarchy)
-            {
-                selectedPlatform[i].SetActive(true);
-
-                Collider platformCol = selectedPlatform[i].GetComponent<Collider>();
-                if (platformCol != null)
-                    // 플랫폼의 크기만큼 위치 조정
-                    lastPlatformPosition += new Vector3(0, 0, platformCol.bounds.size.z);
-
-                // 플랫폼 위치 조정
-                selectedPlatform[i].transform.position = lastPlatformPosition;
-
-                // 비활성화 코루틴 호출
-                StartCoroutine(PlatformOff(selectedPlatform[i]));
-            }
-
-            yield return new WaitForSeconds(0.1f);
+            var oneLeftPlatform = Instantiate(oneLeft, oneLeftGroup.transform);
+            oneLeftPlatform.SetActive(false);
+            PlatformList.Add(oneLeftPlatform);
         }
+
+        yield return new WaitForSeconds(0.1f);
     }
 
-    private IEnumerator PlatformOff(GameObject platform)
+    IEnumerator CreateOneRightPool()
     {
-        yield return new WaitForSeconds(2f); // 2초 대기
-        platform.SetActive(false); // 플랫폼 비활성화
+        GameObject oneRightGroup = new GameObject("OneRightGroup");
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            var oneRightPlatform = Instantiate(oneRight, oneRightGroup.transform);
+            oneRightPlatform.SetActive(false);
+            PlatformList.Add(oneRightPlatform);
+        }
+
+        yield return new WaitForSeconds(0.1f);
     }
 
-    // 다음으로 넘어갈 때 false일 때  마지막에 비활성화 이벤트 액션 delegate
+    public GameObject GetPlatform()
+    {
+        foreach (GameObject platform in PlatformList)
+        {
+            if (!platform.activeSelf)
+                return platform;
+        }
+
+        return null;
+    }
+
+    public void RetunPlatformPool(GameObject platform)
+    {
+        platform.SetActive(false);
+    }
+
 }
