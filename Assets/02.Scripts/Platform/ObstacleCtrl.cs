85715b70716a03f8e7b38d7865054e3575d213d0
tube0330 <zien0516@naver.com>
2024-10-08 11:53:21 +0900

    .

--- Assets/02.Scripts/Platform/ObstacleCtrl.cs
+++ Assets/02.Scripts/Platform/ObstacleCtrl.cs
@@ -14,5 +14,10 @@ public class ObstacleCtrl : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
+        //draw raycast
+        if (Physics.Raycast(tr.position, Vector3.down, out hit, 0.5f))
+            gameObject.SetActive(true);
+
+        else gameObject.SetActive(false);
     }
 }
