b487d41f8a242afa05248c1e11cfec9b6ad822c2
tube0330 <zien0516@naver.com>
2024-10-08 12:34:52 +0900

    .

--- Assets/02.Scripts/Platform/ObstacleCtrl.cs
+++ Assets/02.Scripts/Platform/ObstacleCtrl.cs
@@ -18,7 +18,7 @@ public class ObstacleCtrl : MonoBehaviour
         {
             if (hit.collider.CompareTag("PLATFORM"))
             {
-                // PLATFORM이 감지되지 않았을 때 gameObject.SetActive(false)가 된 후 다시 불러왔을 때 계속 비활성화 되어 있는 상태를 방지하기 위한 로직
+                // PLATFORM이 감지되지 않았을 때 gameObject.SetActive(false)가 된 후 다시 불러왔을 때 계속 비활성화 되어 있는 상태를 방지하기 위한 로직(인데 false라 안됨;)
                 if (!gameObject.activeSelf) // 비활성화 상태라면
                 {
                     Debug.Log($"{gameObject.name}이 PLATFORM 감지하여 활성화");
@@ -35,7 +35,7 @@ public class ObstacleCtrl : MonoBehaviour
                 }
             }
         }
-        
+
         else
         {
             // 플랫폼이 없을 때 비활성화
